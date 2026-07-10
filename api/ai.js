521c3acef80b0ebb5608efd1771f0d42a1e2e529
HaoCoder-2007 <trangiahao0703@gmail.com>
2026-07-10 12:57:23 +0700

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
     }
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
 
     const { command, playlistNames } = req.body;
 
